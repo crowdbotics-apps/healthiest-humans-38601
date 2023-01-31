0863639c3b34344e3fbd3959b42d8f3dbc181664
Crowdbotics <team@crowdbotics.com>
2023-01-31 14:43:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "healthiest-humans-38601",
+  url: "https://healthiest-humans-38601.botics.co",
+  api: "https://healthiest-humans-38601.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
